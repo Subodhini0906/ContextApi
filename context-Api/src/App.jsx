ac0ad4216cdb8826adfa68fa8709636af47f599c
Subodhini <subodhiniaggarwal@gmail.com>
2024-11-24 12:38:48 +0530

    first commit

---
+++ context-Api/src/App.jsx
@@ -0,0 +1,15 @@
+
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Context-api</h1>
+    </>
+  )
+}
+
+export default App
