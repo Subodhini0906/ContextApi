fb22a48b79cdfec68c882a56d88825f75433f319
Subodhini <subodhiniaggarwal@gmail.com>
2024-11-29 22:59:33 +0530

    components

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,11 +1,25 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
+  const [themeMode,setThemeMode]=useState("light")
+  const lightTheme=()=>{
+    setThemeMode("light")
+  }
+  const darkTheme=()=>{
+    setThemeMode("dark")
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
 
   return (
     <>
-
+    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -17,7 +31,7 @@ function App() {
           </div>
         </div>
       </div>
-
+    </ThemeProvider>
 
     </>
   )
